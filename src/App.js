1f0677fc55b04e7a6900ec9594bbeb3be4f0d62d
icpmtech <mourao.martins@gmail.com>
2024-01-21 13:48:00 +0000

    add message

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Button } from 'antd';
-
+import Setup from './setup'
 const App = () => (
   <div className="App">
+    <Setup></Setup>
     <Button type="primary">Button</Button>
   </div>
+
 );
 
 export default App;
